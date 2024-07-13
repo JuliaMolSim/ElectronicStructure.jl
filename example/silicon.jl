76a4086ce13b4eeefcfab5a78c0e70ed8e28a302
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2024-07-13 09:09:20 -0500

    Revert example

--- example/silicon.jl
+++ example/silicon.jl
@@ -1,6 +1,5 @@
 using ASEconvert
 using AtomsBase
-using AtomsCalculators
 using DFTK
 using ElectronicStructure
 
@@ -11,9 +10,6 @@ system = attach_psp(system; Si="hgh/lda/Si-q4")
 params = DftkParameters(; system, functionals=[:lda_x, :lda_c_pw],
                         basis_kwargs=(; Ecut=15, kgrid=(4, 4, 4)),
                         scf_kwargs=(; tol=1e-8))
-calc = DftkCalculator(params)
 
-state = calculate(calc, calc.state.params)
+state = calculate(DftkCalculator(), params)
 @show energy(state)
-
-@show potential_energy(system, calc)
